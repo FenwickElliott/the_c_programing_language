28bb871c6b4f0a95967b4ebf035af30c7091b1f0
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-09-07 12:14:53 -0400

    fahr to celcius

--- c_prog_lang/main.c
+++ c_prog_lang/main.c
@@ -8,7 +8,26 @@
 
 #include <stdio.h>
 
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    printf("Hello, World!\n");
+//    return 0;
+//}
+
 main()
 {
-    printf("Hello, world!\n");
+    int fahr, celsius;
+    int lower, upper, step;
+    
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    fahr = lower;
+    printf("fahr\tcelsius\n");
+    while (fahr <= upper){
+        celsius = 5 * (fahr-32)/9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
 }
