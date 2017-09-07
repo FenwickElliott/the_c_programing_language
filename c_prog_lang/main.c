28ffee9d3d251b1e2d8caa0faffd994a6f740bd3
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-09-07 12:50:58 -0400

    #define symbolic constants

--- c_prog_lang/main.c
+++ c_prog_lang/main.c
@@ -8,33 +8,9 @@
 
 #include <stdio.h>
 
-
-//main()
-//{
-//    float celsius;
-//    int fahr, lower, upper, step;
-//    
-//    lower = 0;
-//    upper = 300;
-//    step = 20;
-//    
-//    fahr = lower;
-//    printf("fahr\tcelsius\n");
-//    while (fahr <= upper){
-//        celsius = (5.0/9.0) * (fahr-32.0);
-//        printf("%d\t%.2f\n", fahr, celsius);
-//        fahr = fahr + step;
-//    }
-//}
+#define EATER "Cat"
+#define MEAL "Fish"
 
 main(){
-    int celsius = 0, fahr;
-
-    printf("cel\tfahr\n");
-
-    while(celsius <= 100){
-        fahr = (9.0/5.0) * celsius + 32;
-        printf("%d\t%d\n", celsius, fahr);
-        celsius = celsius + 5;
-    }
+    printf("A %s is going to eat a %s\n", EATER, MEAL);
 }
