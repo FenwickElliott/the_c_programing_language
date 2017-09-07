7ea0e41fa92ab0f3ad9e4e8e2650f3cffee6096a
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-09-07 11:10:32 -0400

    Initial Commit

---
+++ c_prog_lang/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c_prog_lang
+//
+//  Created by Charles Fenwick Elliott on 9/7/17.
+//  Copyright © 2017 Charles Fenwick Elliott. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
